741097e490e4f8d0ef4e0d16889eacf3099520c5
“Yamil” <cruzyamil17@gmail.com>
2023-09-29 20:05:11 -0300

    add:popup

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,31 +1,3 @@
-// class Popups {
-//     constructor(nombre, descripcion, direccion, localidad, provincia, contacto) {
-//         this.popup = L.popup({keepInView:true,closeButton:false})
-//         .setContent("<h1>" + nombre + "</h1><p>" + descripcion + "</p><h2>Direccion</h2><p>" + direccion + "</p><h2>Localidad</h2><p>" + localidad + "</p><h2>Provincia</h2><p>" + provincia + "</p><h2>Contacto</h2><p>" + contacto + "</p>");
-//     }
-// }
-
-// // var map = L.map('map').setView([51.505, -0.09], 13);
-// // L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-// //     maxZoom: 19,
-// //     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-// // }).addTo(map);
-
-// // var marker = L.marker([50.4501, 30.5234],
-// //     {alt: 'Kyiv'}).addTo(map) // "Kyiv" is the accessible name of this marker
-// //     .bindPopup('Kyiv, Ukraine is the birthplace of Leaflet!').addTo(marker);
-
-// var popup = new Popups("Prueba", "desc", "dir 123", "local", "chubut", "1234567");
-
-// document.getElementById('btnPrueba').addEventListener('click', function(e){
-//     document.getElementById('popupPrueba').innerHTML=popup;
-//     console.log(popup);
-// })
-
-/* eslint-disable no-undef */
-/**
- * Simple map
- */
 
 // config map
 let config = {
@@ -38,32 +10,158 @@ const zoom = 4;
 const lat = -44.44;
 const lng = -68.06;
 
-// coordinate array with popup text
-let points = [
-    [-34.5419214203672, -58.4426215946441, "IAFE/CIMA/DCG/IIPA/DNGAAyEA/IGEBA/INQUIMAE/PNA"],
-    [-38.0313568540783, -57.5322375431191, "INIDEP"],
-    [-38.6658512007599, -62.2343722737833, "IADO"],
-    [-40.6286873079791, -64.187055309402, "CIMAS"],
-    [-42.6627066637157, -65.004170827596, "CESIMAR"],
-    [-45.8254438633127, -67.4633783451962, "CIT - GOLFO SAN JORGE"],
-    [-51.5330642603803, -69.248236391595, "CIT - SANTA CRUZ"],
-    [-54.7426387444932, -68.3616337812203, "CADIC"],
-    [-62.2377201064155, -58.6670725759177, "IAA BASE CARLINI"],
-  ];
 
-// calling map
 const map = L.map("map", config).setView([lat, lng], zoom);
 
-// Used to load and display tile layers on the map
-// Most tile servers require attribution, which you can set under `Layer`
+
+// calling map
+
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  attribution:
+  '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map); 
 
-// loop that adds many markers to the map
-for (let i = 0; i < points.length; i++) {
-    const [lat, lng, popupText] = points[i];
-  
-    marker = new L.marker([lat, lng]).bindPopup(popupText).addTo(map);
+
+var pane = map.createPane("fixed", document.getElementById("map"));
+
+
+// Coordenadas de los puertos
+
+const points = [
+  {
+    lat: -34.5419214203672,
+    lng: -58.4426215946441, 
+    text: "<h3>IAFE/CIMA/DCG/IIPA/DNGAAyEA/IGEBA/INQUIMAE/PNA😀</h3><br> + Agregar mas contenido.",
+  },
+  {
+    lat: -38.0313568540783,
+    lng: -57.5322375431191,
+    text: "<h3>INIDEP 😀</h3><br>Grab the lower right corner and reduce the width of the map.",
+  },
+  {
+    lat:-38.6658512007599,
+    lng: -62.2343722737833,
+    text: "<h3>CIMAS 😀</h3><br>Grab the lower right corner and reduce the width of the map.",
+  },
+  {
+    lat:-42.6627066637157,
+    lng: -65.004170827596,
+    text: "<h3>CESIMAR 😀</h3><br>Grab the lower right corner and reduce the width of the map.",
+  },
+  {
+    lat:-45.8254438633127,
+    lng: -67.4633783451962,
+    text: "<h3>CIT - GOLFO SAN JORGE 😀</h3><br>Grab the lower right corner and reduce the width of the map.",
+  },
+  {
+    lat: -51.5330642603803,
+    lng: -69.248236391595,
+    text: "<h3>CIT - SANTA CRUZ 😀</h3><br>Grab the lower right corner and reduce the width of the map.",
+  },
+  {
+    lat: -54.7426387444932,
+    lng: -68.3616337812203,
+    text: "<h3>CADIC 😀</h3><br>Grab the lower right corner and reduce the width of the map.",
+  },
+  {
+    lat: -62.2377201064155,
+    lng:  -58.6670725759177,
+    text: "<h3>, IAA BASE CARLINI 😀</h3><br>Grab the lower right corner and reduce the width of the map.",
   }
+];
+
+// coordinate array with popup text
+// let points = [
+//     [-34.5419214203672, -58.4426215946441, "IAFE/CIMA/DCG/IIPA/DNGAAyEA/IGEBA/INQUIMAE/PNA"],
+//     [-38.0313568540783, -57.5322375431191, "INIDEP"],
+//     [-38.6658512007599, -62.2343722737833, "IADO"],
+//     [-40.6286873079791, -64.187055309402, "CIMAS"],
+//     [-42.6627066637157, -65.004170827596, "CESIMAR"],
+//     [-45.8254438633127, -67.4633783451962, "CIT - GOLFO SAN JORGE"],
+//     [-51.5330642603803, -69.248236391595, "CIT - SANTA CRUZ"],
+//     [-54.7426387444932, -68.3616337812203, "CADIC"],
+//     [-62.2377201064155, -58.6670725759177, "IAA BASE CARLINI"],
+//   ];
+//
+
+// create new div icon width svg
+const newIcon = L.divIcon({
+  className: "marker",
+  iconSize: [40, 40],
+  iconAnchor: [12, 24],
+  popupAnchor: [700, -16],
+});
+
+
+points.map(({ lat, lng, text }) => {
+  // create marker and add to map
+  const marker = L.marker([lat, lng], {
+    // icon: newIcon,
+  }).addTo(map);
+
+  // crewate popup, set contnet
+  const popup = L.popup({
+    pane: "fixed",
+    className: "popup-fixed test",
+    autoPan: false,
+  }).setContent(text);
+
+  marker.bindPopup(popup).on("click", fitBoundsPadding);
+});
+
+// remove all animation class when popupclose
+map.on("popupclose", function (e) {
+  removeAllAnimationClassFromMap();
+});
+
+// ------------------------------------------------
+
+const mediaQueryList = window.matchMedia("(min-width: 700px)");
+
+mediaQueryList.addEventListener("change", (event) => onMediaQueryChange(event));
+
+onMediaQueryChange(mediaQueryList);
+
+function onMediaQueryChange(event) {
+  if (event.matches) {
+    document.documentElement.style.setProperty("--min-width", "true");
+  } else {
+    document.documentElement.style.removeProperty("--min-width");
+  }
+}
+
+function fitBoundsPadding(e) {
+  removeAllAnimationClassFromMap();
+  // get with info div
+  const boxInfoWith = document.querySelector(
+    ".leaflet-popup-content-wrapper"
+  ).offsetWidth;
+
+  // add class to marker
+  e.target._icon.classList.add("animation");
+
+  // create a feature group, optionally given an initial set of layers
+  const featureGroup = L.featureGroup([e.target]).addTo(map);
+
+  // check if attribute exist
+  const getPropertyWidth =
+    document.documentElement.style.getPropertyValue("--min-width");
+
+  // sets a map view that contains the given geographical bounds
+  // with the maximum zoom level possible
+  map.fitBounds(featureGroup.getBounds(), {
+    paddingTopLeft: [getPropertyWidth ? -boxInfoWith : 0, 10],
+  });
+  document.querySelector('.leaflet-popup').style.position= "fixed";
+}
+
+function removeAllAnimationClassFromMap() {
+  // get all animation class on map
+  const animations = document.querySelectorAll(".animation");
+  animations.forEach((animation) => {
+    animation.classList.remove("animation");
+  });
+
+  // back to default position
+  map.setView([lat, lng], zoom);
+}
