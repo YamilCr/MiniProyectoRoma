e5be7cf2704ad4293402e99f8de01bc45905245b
Pablo Rosales <pablo27207@gmail.com>
2023-10-05 16:17:32 -0300

    mapa de IGN y algunos detalles

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,8 +1,8 @@
 
 // config map
 let config = {
-    minZoom: 0,
-    maxZoom: 18,
+    minZoom: 1,
+    maxZoom: 20,
   };
 // magnification with which the map will start
 const zoom = 4;
@@ -15,11 +15,13 @@ const map = L.map("map", config).setView([lat, lng], zoom);
 
 
 // calling map
-
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution:
-  '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map); 
+var argenmap = new L.tileLayer('https://wms.ign.gob.ar/geoserver/gwc/service/tms/1.0.0/capabaseargenmap@EPSG%3A3857@png/{z}/{x}/{-y}.png', {
+    minZoom: 1, maxZoom: 20
+}).addTo(map);
+//L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+ // attribution:
+ // '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+//}).addTo(map); 
 
 
 var pane = map.createPane("fixed", document.getElementById("map"));
@@ -31,58 +33,44 @@ const points = [
   {
     lat: -34.5419214203672,
     lng: -58.4426215946441, 
-    text: "<h3>IAFE/CIMA/DCG/IIPA/DNGAAyEA/IGEBA/INQUIMAE/PNA😀</h3><br> + Agregar mas contenido.",
+    text: "<h3>IAFE/CIMA/DCG/IIPA/DNGAAyEA/IGEBA/INQUIMAE/PNA</h3><br> + Agregar mas contenido.",
   },
   {
     lat: -38.0313568540783,
     lng: -57.5322375431191,
-    text: "<h3>INIDEP 😀</h3><br>Grab the lower right corner and reduce the width of the map.",
+    text: "<h3>INIDEP</h3><br>Grab the lower right corner and reduce the width of the map.",
   },
   {
     lat:-38.6658512007599,
     lng: -62.2343722737833,
-    text: "<h3>CIMAS 😀</h3><br>Grab the lower right corner and reduce the width of the map.",
+    text: "<h3>CIMAS</h3><br>Grab the lower right corner and reduce the width of the map.",
   },
   {
     lat:-42.6627066637157,
     lng: -65.004170827596,
-    text: "<h3>CESIMAR 😀</h3><br>Grab the lower right corner and reduce the width of the map.",
+    text: "<h3>CESIMAR</h3><br>Grab the lower right corner and reduce the width of the map.",
   },
   {
     lat:-45.8254438633127,
     lng: -67.4633783451962,
-    text: "<h3>CIT - GOLFO SAN JORGE 😀</h3><br>Grab the lower right corner and reduce the width of the map.",
+    text: "<h3>CIT - GOLFO SAN JORGE</h3><br>Grab the lower right corner and reduce the width of the map.",
   },
   {
     lat: -51.5330642603803,
     lng: -69.248236391595,
-    text: "<h3>CIT - SANTA CRUZ 😀</h3><br>Grab the lower right corner and reduce the width of the map.",
+    text: "<h3>CIT - SANTA CRUZ</h3><br>Grab the lower right corner and reduce the width of the map.",
   },
   {
     lat: -54.7426387444932,
     lng: -68.3616337812203,
-    text: "<h3>CADIC 😀</h3><br>Grab the lower right corner and reduce the width of the map.",
+    text: "<h3>CADIC</h3><br>Grab the lower right corner and reduce the width of the map.",
   },
   {
     lat: -62.2377201064155,
     lng:  -58.6670725759177,
-    text: "<h3>, IAA BASE CARLINI 😀</h3><br>Grab the lower right corner and reduce the width of the map.",
+    text: "<h3>, IAA BASE CARLINI</h3><br>Grab the lower right corner and reduce the width of the map.",
   }
 ];
-
-// coordinate array with popup text
-// let points = [
-//     [-34.5419214203672, -58.4426215946441, "IAFE/CIMA/DCG/IIPA/DNGAAyEA/IGEBA/INQUIMAE/PNA"],
-//     [-38.0313568540783, -57.5322375431191, "INIDEP"],
-//     [-38.6658512007599, -62.2343722737833, "IADO"],
-//     [-40.6286873079791, -64.187055309402, "CIMAS"],
-//     [-42.6627066637157, -65.004170827596, "CESIMAR"],
-//     [-45.8254438633127, -67.4633783451962, "CIT - GOLFO SAN JORGE"],
-//     [-51.5330642603803, -69.248236391595, "CIT - SANTA CRUZ"],
-//     [-54.7426387444932, -68.3616337812203, "CADIC"],
-//     [-62.2377201064155, -58.6670725759177, "IAA BASE CARLINI"],
-//   ];
-//
 
 // create new div icon width svg
 const newIcon = L.divIcon({
@@ -99,7 +87,7 @@ points.map(({ lat, lng, text }) => {
     // icon: newIcon,
   }).addTo(map);
 
-  // crewate popup, set contnet
+  // create popup, set content
   const popup = L.popup({
     pane: "fixed",
     className: "popup-fixed test",
